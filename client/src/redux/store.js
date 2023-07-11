841d36f2b1e39e04a6c90ed7ddea1bd59ba139ea
guptaPrateek10 <guptaPrateek10@gmail.com>
2023-07-12 00:47:39 +0530

    Redux Persist feature added.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./cartSlice";
 import userReducer from "./userSlice";
-export default configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer,
-  },
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const rootReducers = combineReducers({ cart: cartReducer, user: userReducer });
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
+
+export let persistor = persistStore(store);
